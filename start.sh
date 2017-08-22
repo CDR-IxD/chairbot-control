9ead72ae3776ca232fcf1ca89057b0ef50cb2928
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-08-21 20:54:13 -0700

    Low quality but working

--- start.sh
+++ start.sh
@@ -5,9 +5,9 @@ xterm -e "cd ./webcam_stream && node websocket-relay.js supersecret 8081 8082" &
 xterm -e "http-server ./webcam_stream/" &
 xterm -e ffmpeg \
 	-f v4l2 \
-		-framerate 25 -video_size 640x480 -i /dev/video0 \
+		-framerate 20 -video_size 300x225 -i /dev/video0 \
 	-f mpegts \
-		-codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 \
+		-codec:v mpeg1video -s 300x225 -b:v 500k -bf 0 \
 	http://localhost:8081/supersecret &
 
 xterm -e "cd ./neato-interface-server && node interface-server.js"
