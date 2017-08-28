68e48e710653e693cfae112825301235996b781f
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-08-28 14:28:51 -0700

    Working commit

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 rm -rf out.avi
+#xterm -e "cd ./neato-interface-server && node interface-server.js" &
 xterm -e "cd ./webcam_stream && node websocket-relay.js supersecret 8081 8082" &
 xterm -e "http-server ./webcam_stream/" &
 xterm -e ffmpeg \
 	-f v4l2 \
-		-framerate 20 -video_size 300x225 -i /dev/video0 \
+		-framerate 20 -video_size 300x225 -i /dev/video1 \
 	-f mpegts \
 		-codec:v mpeg1video -s 300x225 -b:v 500k -bf 0 \
-	http://localhost:8081/supersecret &
+	http://localhost:8081/supersecret 
 
-xterm -e "cd ./neato-interface-server && node interface-server.js"
